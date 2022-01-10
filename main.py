ef0276b6ab1cef76e08f069e3cb0a8797ca88e91
Husarov Maksym <ketayon@gmail.com>
2022-01-10 14:03:58 +0200

    Response Model

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 
+class PackageIn(BaseModel):
+    secret_id: int
+    name: str
+    number: str
+    description: Optional[str] = None
+
 class Package(BaseModel):
     name: str
     number: str
@@ -14,6 +20,6 @@ app = FastAPI()
 async def hello_world():
     return {'Hello': 'world.'}
 
-@app.post("/package/{priority}")
-async def make_package(priority: int, package: Package, value: bool):
-    return {"priority": priority, **package.dict(), "value": value}
+@app.post('/package/', response_model=Package, response_model_include={"description"})
+async def make_package(package: PackageIn):
+    return {**package.dict()}
